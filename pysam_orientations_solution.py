e07b60fbaa3c0b50a418e6a649fd118ec907caae
Lila14 <lr65358@gmail.com>
2016-02-03 11:53:43 -0500

    Create pysam_orientations_solution.py
    Solution to mate-pair orientation frequencies exercise. Do not read until you're done with the exercise.

---
+++ pysam_orientations_solution.py
@@ -0,0 +1,30 @@
+import pysam as ps
+
+samfile = ps.AlignmentFile("hESC_chr22.bam", "rb", check_sq=False)
+fr=0	#convergent count
+rf=0	#divergent count
+ff=0	#parallel count
+lineCount=0
+currPercentage=0	#to track progress
+for read in samfile:
+	if read.next_reference_start>read.reference_start:
+		mateAhead=True
+	else:
+		mateAhead=False
+		if (not read.is_reverse and read.mate_is_reverse and mateAhead) or (read.is_reverse and not read.mate_is_reverse and not mateAhead):	
+			fr+=1
+		elif (read.is_reverse and not read.mate_is_reverse and mateAhead) or (not read.is_reverse and read.mate_is_reverse and not mateAhead):	
+			rf+=1
+		else:
+			ff+=1
+	lineCount+=1
+	newPercentage = currPercentage+1
+	if float(lineCount)/8672163 >= float(newPercentage)/100: #if at least X% of the file has been read, print percentage	
+		currPercentage = newPercentage
+		print str(currPercentage) + "% complete"
+samfile.close()
+
+total=fr+rf+ff
+print "{0:.1f}% convergent".format(float(fr)/total*100)
+print "{0:.1f}% divergent".format(float(rf)/total*100)
+print "{0:.1f}% parallel".format(float(ff)/total*100)
